a66275fd7d59288c372c9ccacc8e93400d5a7dae
Cooper Whitlow <cooperwhitlow@gmail.com>
2017-01-30 04:34:16 -0800

    commit first working version

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Plug the Pug Interactive</title>
+	<script src="js/script.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="main.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<meta name="viewport" content="width=750">
+</head>
+<body onload="setTimeout('init(0,25,1);', 100);">
+	<div id="content-background">
+	</div>
+		<div id="content-wrapper">
+			<img class="face" id="face-left" src="media/face-left.png" alt=""/>
+			<div class="content-box" id="content-box-left">
+				<h1>SOUNDBOARD</h1>
+				<div id="all-speaker-wrapper">
+					<div class="single-speaker-wrapper">
+						<img alt="" class="speaker-paused speaker-left" src="media/speaker-paused.png"/>
+						<img alt="" class=" speaker-playing speaker-left" src="media/speaker-playing.png"/>
+						<h3 id="single-speaker-left-caption">BREATH</h3>
+						<audio id="breath-audio" src="media/breath.mp3" preload loop></audio>
+					</div>
+					<div class="single-speaker-wrapper">
+						<img alt="" class="speaker-paused speaker-middle" src="media/speaker-paused.png"/>
+						<img alt="" class="speaker-playing speaker-middle" src="media/speaker-playing.png"/>
+						<h3 id="speaker-middle-caption">EAT</h3>
+						<audio id="eat-audio" src="media/eat.mp3" preload loop></audio>
+					</div>
+					<div class="single-speaker-wrapper">
+						<img alt="" class="speaker-paused speaker-right" src="media/speaker-paused.png"/>
+						<img alt="" class="speaker-playing speaker-right" src="media/speaker-playing.png"/>
+						<h3 id="speaker-right-caption">SLEEP</h3>
+						<audio id="sleep-audio" src="media/sleep.mp3" preload loop></audio>
+					</div>
+				</div>
+			</div>
+			<img alt="" class="face" id="face-middle" src="media/face-middle.png"/>
+			<div class="content-box" id="content-box-middle">
+				<h1>FEED THE PLUG</h1>
+				<img alt="" class="live-stream" id="mjpeg_dest" src="" />
+				<div id="button-wrapper">
+					<div class="buttons" id="treat-drop-button">FEED TREAT!</div>
+					<audio id="treat-drop-audio" src="media/boxing-bell-sound.mp3" volume="0.8" onended="$('#treat-drop-audio').remove()" preload></audio>
+					<a href="http://a.co/fbM3X6v" rel="noopener noreferrer" target="_blank">
+						<div class="buttons" id="treat-donate-button">DONATE</div>
+					</a>
+				</div>
+				<p id="treat-notification">You dropped a treat! Watch to see if Plug comes to snatch it up. If not...quiet! He may be sleeping : )</p>
+			</div>
+			<img alt="" class="face" id="face-right" src="media/face-right.png"/>
+			<div class="content-box" id="content-box-right">
+				<a id="ig-link" href="https://www.instagram.com/plugthepug/">
+					<img alt="" src="/media/ig-custom.png"/>
+				</a>
+			</div>
+		</div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,116 @@
+//begin jquery after DOM loaded
+$( document ).ready(function() {
+
+	//declare variable(s) for later use
+	var currentTime = formatTimeOfDay($.now());
+
+	//page setup
+	$( "#content-box-left" ).hide();
+	$( "#content-box-middle" ).hide();
+	$( "#content-box-right" ).hide();
+
+	//click-to-zoom for video
+	$( "#mjpeg_dest" ).click(function() {
+		$( this ).toggleClass("zoom-video");
+	});
+
+	//click events for background
+	$( "#content-background").click(function() {
+		$(".content-box").hide();
+		$( "#face-left" ).removeClass("face-active").fadeIn();
+    	$( "#face-middle" ).removeClass("face-active").fadeIn();
+    	$( "#face-right" ).removeClass("face-active").fadeIn();
+    	$( ".face" ).each(function() {
+    		var oldSrc = $( this ).attr("src");
+    		var newSrc = oldSrc.replace(/-photo.png/g, ".png");
+    		$( this ).attr("src", newSrc);
+    	});
+    });
+
+	//insert univeral error message for HTML5 media tracks
+	$( "audio" ).html("Your browser doesn't support pug sounds : ( Update it yo!");
+	
+	//hover events for pug faces
+    $( "#face-left" ).hover(function() {
+    	$( this ).attr("src","media/face-left-photo.png");
+	}, function() {
+		var isClicked = $( this ).hasClass("face-active");
+		if ( !isClicked ) { 
+			$( this ).attr("src","media/face-left.png");
+		}
+	});
+
+    $( "#face-middle" ).hover(function() {
+    	$( this ).attr("src","/media/face-middle-photo.png");
+	}, function() {
+		var isClicked = $( this ).hasClass("face-active");
+		if ( !isClicked ) { 
+			$( this ).attr("src","media/face-middle.png");
+		}
+	});
+
+    $( "#face-right" ).hover(function() {
+    	$( this ).attr("src","/media/face-right-photo.png");
+    }, function() {
+		var isClicked = $( this ).hasClass("face-active");
+		if ( !isClicked ) { 
+			$( this ).attr("src","media/face-right.png");
+		}
+	});
+
+	//click events for pug faces
+    $( "#face-left" ).click(function() {
+    	$( "#content-box-left" ).fadeIn();
+    	$( ".speaker-playing" ).hide();
+    	$( "#face-middle" ).hide();
+    	$( "#face-right" ).hide();
+    	$( this ).attr("src","media/face-left-photo.png");
+    	$( this ).addClass("face-active")
+    });
+
+    $( "#face-middle" ).click(function() {
+    	$( "#content-box-middle" ).fadeIn();
+    	$( "#treat-notification").hide();
+    	$( "#face-left" ).hide();
+    	$( "#face-right" ).hide();
+    	$( this ).attr("src","media/face-middle-photo.png");
+    	$( this ).addClass("face-active")
+    });
+
+    $( "#face-right" ).click(function() {
+    	$( "#content-box-right" ).fadeIn();
+    	$( "#face-middle" ).hide();
+    	$( "#face-left" ).hide();
+    	$( this ).attr("src","media/face-right-photo.png");
+    	$( this ).addClass("face-active")
+    });
+
+	//click events for soundboard
+	$( ".speaker-paused" ).click(function() {
+		$( this ).toggle();
+		$( this ).next().toggle();
+		$( this ).siblings().last().trigger("play");
+
+	});
+
+	$( ".speaker-playing" ).click(function() {
+		$( this ).toggle();
+		$( this ).prev().toggle();
+		$( this ).siblings().last().trigger("pause");
+	});
+
+	//click events for treats
+	function formatTimeOfDay(millisSinceEpoch) {
+  		var secondsSinceEpoch = (millisSinceEpoch / 1000) | 0;
+  		var secondsInDay = ((secondsSinceEpoch % 86400) + 86400) % 86400;
+  		var minutes = ((secondsInDay / 60) | 0) % 60;
+  		var hours = (secondsInDay / 3600 - 8) | 0;
+  		return hours + (minutes < 10 ? ":0" : ":") + minutes;
+	}
+
+	$( "#treat-drop-button" ).click(function() {
+		$( "#treat-drop-audio" ).trigger("play");
+		$( this ).html("FED @ " + currentTime);
+		$( "#treat-notification" ).slideDown();
+	});
+});
